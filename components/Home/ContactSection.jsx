524402298026f0b0820a96f6035ea6396143f4b5
InternetMadeCoder <anonymous210705@gmail.com>
2025-03-27 01:28:56 +0530

    .

--- components/Home/ContactSection.jsx
+++ components/Home/ContactSection.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const ContactSection = () => {
-    return (
-        <div className="py-16 px-4 sm:px-6 md:px-12 lg:px-20 xl:px-28 bg-white flex flex-col items-start gap-4 relative z-30 md:items-center white-section">
-            <h2 className="text-4xl">CONTACT US</h2>
-            <p className="font-medium text-lg">Connect with us for inquiries or collaboration</p>
-            <Link 
-              href="/contact-us"
-              className="relative inline-block px-8 py-4 bg-[#EF9535] text-white font-semibold overflow-hidden before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-transparent before:via-white/20 before:to-transparent before:transition-all before:duration-500 hover:before:left-full"
-            >
-              Read More
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="py-16 px-4 sm:px-6 md:px-12 lg:px-20 xl:px-28 bg-white flex flex-col items-start gap-4 relative z-30 md:items-center white-section">
+      <h2 className="text-4xl">CONTACT US</h2>
+      <p className="font-medium text-lg">
+        Connect with us for inquiries or collaboration
+      </p>
+      <Link
+        href="/contact-us"
+        className="relative inline-block px-8 py-4 bg-[#EF9535] text-white font-semibold overflow-hidden before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-transparent before:via-white/20 before:to-transparent before:transition-all before:duration-500 hover:before:left-full"
+      >
+        Read More
+      </Link>
+    </div>
+  );
+};
 
-export default ContactSection
+export default ContactSection;
--- components/Home/GallerySection.jsx
+++ components/Home/GallerySection.jsx
@@ -13,7 +13,9 @@ const GallerySection = () => {
 
   return (
     <div className="py-4 md:py-8 flex flex-col items-center gap-4 md:gap-8 relative z-30 bg-white">
-      <h2 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">OUR GALLERY</h2>
+      <h2 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">
+        OUR GALLERY
+      </h2>
       <div className="film-strip-container w-screen overflow-hidden">
         <div className="film-strip">
           {[...images, ...images].map((image, index) => (
@@ -39,7 +41,7 @@ const GallerySection = () => {
           ))}
         </div>
       </div>
-      <Link 
+      <Link
         href="/gallery"
         className="relative inline-block px-8 py-4 bg-[#EF9535] text-white font-semibold overflow-hidden before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-transparent before:via-white/20 before:to-transparent before:transition-all before:duration-500 hover:before:left-full"
       >
